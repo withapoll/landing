6f777263caf9c711709d132df76697e69c68bf66
max28p <97313617+max28p@users.noreply.github.com>
2024-12-11 20:57:00 +0300

    сделал popup

--- js_php.js
+++ js_php.js
@@ -29,6 +29,10 @@ function buttonPhp () {
     const name = document.getElementById("name").value;
     const phoneNumber = document.getElementById("phone").value;
     const email = document.getElementById("email").value;
+    const popUpContainer = document.getElementById("pop-up");
+    const popUpAct = document.getElementById("pop-up-active");
+    const popUpErr = document.getElementById("pop-up-error");
+
 
     const data = JSON.stringify({name, phoneNumber, email});
 
@@ -39,4 +43,36 @@ function buttonPhp () {
         },
         body: data
     })
+
+    fetch('server.php')
+        .then(response => {
+            if (response.ok) {
+                popUpContainer.classList.add('open');
+                popUpAct.classList.add('open-act');
+            } else {
+                popUpContainer.classList.add('open');
+                popUpErr.classList.add('open-err');
+            }
+        })
     }
+
+const exitButton = document.getElementById("exit");
+const popUpContainer = document.getElementById("pop-up");
+
+exitButton.addEventListener('click', () =>{
+    popUpContainer.classList.remove('open');
+
+    document.getElementById("name").value = '';
+    document.getElementById("phone").value = '';
+    document.getElementById("email").value = '';
+    const button = document.getElementById('button-color');
+    const button2 = document.getElementById('button-color2');
+
+    button.classList.remove('active');
+    button.classList.add('disabled');
+    button.disabled = true;
+
+    button2.classList.remove('active');
+    button2.classList.add('disabled');
+    button2.disabled = true;
+});
